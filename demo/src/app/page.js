9e5176aaff4818339f3d956592205b4448ac7941
Z1CoL <zcol4615@gmail.com>
2025-09-02 23:19:12 +0800

    gerte zurg edriin soliv

--- demo/src/app/page.js
+++ demo/src/app/page.js
@@ -3,22 +3,22 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className=" w-full h-full rounded-4xl flex flex-wrap ml-10">
+      {/* head */}
+      <div className="">{/* videos */}</div>
       <div className="flex gap-[24px] h-[347]">
         <div className="w-[100%] h-[226px] mt-[24px]">
-          <div className="w-[419px] h-full bg-white rounded-[10px]"></div>
+          <div className="w-[419px] h-full bg-[url(./img/L1.jpg)] bg-cover bg-no-repeat rounded-[10px]"></div>
 
-          <div className="flex mt-[12px] place-content-between">
+          <div className="flex mt-[12px]">
             <div className="bg-[url(./img/shorts.jpeg)] mr-[12px] w-[36px] h-[36px] bg-cover rounded-full"></div>
             <div className="h-[85px]">
-              <p className="font-bold">
-                Mix - Rock That Body - The Black Eyed Peas
-              </p>
+              <p className="font-bold">Animation universe - The LADDER</p>
               <p className="text-gray-500 text-sm  ">Silo Entertainment</p>
               <p className="text-gray-500 text-sm  ">572K views • 4 days ago</p>
             </div>
             <div>
               <svg
-                className="mt-[5px] bi bi-three-dots-vertical "
+                className="mt-[5px] bi bi-three-dots-vertical ml-18"
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
         {/* second video */}
         <div className="w-[100%] h-[226px] mt-[24px]">
-          <div className="w-[419px] h-full bg-white rounded-[10px]"></div>
+          <div className="w-[419px] h-full bg-[url(./img/snake.jpg)] bg-cover rounded-[10px]"></div>
 
           <div className="flex mt-[12px] place-content-between">
             <div className="bg-[url(./img/shorts.jpeg)] mr-[12px] w-[36px] h-[36px] bg-cover rounded-full"></div>
@@ -58,9 +58,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-            <div className="flex gap-[24px] h-[347]">
+      <div className="flex gap-[24px] h-[347]">
         <div className="w-[100%] h-[226px] mt-[24px]">
-          <div className="w-[419px] h-full bg-white rounded-[10px]"></div>
+          <div className="w-[419px] h-full bg-[url(./img/song.jpg)] bg-cover rounded-[10px]"></div>
 
           <div className="flex mt-[12px] place-content-between">
             <div className="bg-[url(./img/shorts.jpeg)] mr-[12px] w-[36px] h-[36px] bg-cover rounded-full"></div>
@@ -87,7 +87,7 @@ export default function Home() {
         </div>
         {/* second video */}
         <div className="w-[100%] h-[226px] mt-[24px]">
-          <div className="w-[419px] h-full bg-white rounded-[10px]"></div>
+          <div className="w-[419px] h-full bg-[url(./img/griffin.jpg)] bg-cover rounded-[10px]"></div>
 
           <div className="flex mt-[12px] place-content-between">
             <div className="bg-[url(./img/shorts.jpeg)] mr-[12px] w-[36px] h-[36px] bg-cover rounded-full"></div>
